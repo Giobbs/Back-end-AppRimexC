25ccc1171efc3c6e07c86d06e943d9c708ddf73b
Giobbs <luc.florio@libero.it>
2025-04-04 09:43:30 +0200

    Add files via upload

---
+++ appCamper/src/main/java/com/rimex/appCamper/AppCamperApplication.java
@@ -0,0 +1,13 @@
+package com.rimex.appCamper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCamperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCamperApplication.class, args);
+	}
+
+}
---
+++ appCamper/src/test/java/com/rimex/appCamper/AppCamperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimex.appCamper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCamperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
